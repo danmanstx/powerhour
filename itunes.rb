3f9809430f8f1101c96540fd5868843a5c5c9660
danmanstx <dnamanstx@gmail.com>
2011-05-04 01:01:38 -0400

    multiple fixes and more user input

--- itunes.rb
+++ itunes.rb
@@ -12,53 +12,86 @@ name_of_playlist = "depression vol 2"
 #framework 'Foundation'
 #framework 'ScriptingBridge'
 
+puts "enter number of players:"
+numplayers = gets.chomp
 
 
-#i = 0
-#bac = []
+numplayer = numplayers.to_i
+i = 0
+$weight = [numplayer]
+$bac    = [numplayer]
+$name   = [numplayer]
+$sex    = [numplayer]
 
-#puts "enter number of players"
-#numplayers = gets.chomp
+while i < numplayer
+  real_num = i + 1
+  puts "enter name for player #{real_num} :"
+  weighttemp = gets.chomp
+  $name[i] = weighttemp
+  i = i + 1
+end
 
-#counter = 0
-#weight = []
+i = 0
 
-#while i < numplayers.to_i
-#  puts "weight for player"
-#  weight[i] = gets.chomp
-#  i = i + 1
+while i < numplayer
+  real_num = 1 + i
+  puts "enter weight for player #{real_num} :"
+  weighttemp = gets.chomp
+  $weight[i] = weighttemp.to_i
+  i = i + 1
+end
+i = 0
+while i < numplayer
+  real_num = i + 1
+  puts "enter sex for player #{real_num} (1 for male, 2 for female):"
+  weighttemp = gets.chomp
+  $sex[i] = weighttemp.to_i
+  i = i + 1
+end
+
+$time = 0;
+
+#def bac_calc(time, numplayer, @weight, @bac)
+#counter = 0 
+#while counter < numplayer
+#  bac[counter] = ( 60 * ( 5.14 / weight[i]) * 0.73) - (time * 0.015)
+#  counter = counter + 1
+#end
 #end
 
-#i = 0
-
-#  while counter < numplayers[i].to_i
-#  bac[i] = ((60 * (5.14/ weight[i].to_f) * 0.73) - 0.015)
-#  counter = counter + 1
-#  i = i + 1
-#  end
-
+def bac_calc( time, sex, weight)
+  if sex == 1
+    coef = 0.73
+  else
+    coef = 0.66
+  end
+  bac = (( (time * 1.5 * 0.05) * (5.14 / weight) * coef ) - ((time/60) * 0.015) )
+  return bac
+end
 
 itunes = SBApplication.applicationWithBundleIdentifier("com.apple.itunes")
 
 itunes.run
 
-
-
-print itunes.currentTrack().name()
-
 playlist = itunes.sources.objectWithName("Library").userPlaylists.objectWithName(name_of_playlist)
 
-time1 = Time.new
-time2 = time1 + 60
 first = 1
 
 for x in 0..60
   if first == 1
     playlist.playOnce(false) if playlist
+    puts "the First track is #{itunes.currentTrack().name()} by #{itunes.currentTrack().artist()}"
+    first = 2
+  else
+    sleep(60)
+    itunes.nextTrack()
+    puts "the current track is #{itunes.currentTrack().name()} by #{itunes.currentTrack().artist()} this is track number #{$time+2}"
+    counter = 0
+    while counter < numplayer
+      $bac[counter] = bac_calc($time, $sex[counter], $weight[counter])
+      puts "BAC for #{$name[counter]} is #{$bac[counter]}"
+      counter = counter + 1
+    end
+    $time = $time + 1
   end
-  sleep(60)
-  first = 2
-  itunes.nextTrack()
-  #bac.each { |x| print x}
-  
 end
